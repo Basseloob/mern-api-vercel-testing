442a7dd0bc8458abf4d852968591b50f0d12bcd7
Basseloob <Bassel.baragaba@gmail.com>
2023-12-12 14:21:17 +0300

    first commit

--- server/index.js
+++ server/index.js
@@ -1,39 +1,41 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const RegisterModel = require('./models/Register')
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const RegisterModel = require("./models/Register");
 
-const app = express()
-app.use(cors(
-    {
-        origin: ["https://deploy-mern-frontend.vercel.app"],
-        methods: ["POST", "GET"],
-        credentials: true
-    }
-));
-app.use(express.json())
+const app = express();
+app.use(
+  cors({
+    origin: ["https://deploy-mern-frontend.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
+app.use(express.json());
 
-mongoose.connect('mongodb+srv://yousaf:test123@cluster0.g4i5dey.mongodb.net/test?retryWrites=true&w=majority');
-
+mongoose.connect(
+  // 'mongodb+srv://yousaf:test123@cluster0.g4i5dey.mongodb.net/test?retryWrites=true&w=majority'
+  "mongodb+srv://basseloob:Basilpsp9111@alhabib-cluster.nnbcxyh.mongodb.net/testname?retryWrites=true&w=majority"
+);
 
 app.get("/", (req, res) => {
-    res.json("Hello");
-})
-app.post('/register', (req, res) => {
-    const {name, email, password} = req.body;
-    RegisterModel.findOne({email: email})
-    .then(user => {
-        if(user) {
-            res.json("Already have an account")
-        } else {
-            RegisterModel.create({name: name, email: email, password: password})
-            .then(result => res.json(result))
-            .catch(err => res.json(err))
-        }
-    }).catch(err => res.json(err))
-})
-
+  res.json("Hello");
+});
+app.post("/register", (req, res) => {
+  const { name, email, password } = req.body;
+  RegisterModel.findOne({ email: email })
+    .then((user) => {
+      if (user) {
+        res.json("Already have an account");
+      } else {
+        RegisterModel.create({ name: name, email: email, password: password })
+          .then((result) => res.json(result))
+          .catch((err) => res.json(err));
+      }
+    })
+    .catch((err) => res.json(err));
+});
 
 app.listen(3001, () => {
-    console.log("Server is Running")
-})
+  console.log("Server is Running");
+});
